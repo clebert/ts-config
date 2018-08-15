e7be91c899db05cc344f4a7e599c1cdb0bd29568
Clemens Akens <clebert@me.com>
2018-08-15 11:27:42 +0200

    build: migrate from make to autosrc

---
+++ autosrc.js
@@ -0,0 +1,38 @@
+const rules = (exports.rules = {});
+
+function update(target) {
+  return `touch ${target}`;
+}
+
+rules.test = {
+  prereqs: ['README.md'],
+  recipe: target => [
+    "yarn prettier --list-different '**/*.{js,json,md,ts,yml}'",
+    "yarn tslint --config tslint.json --project . --format verbose '**/*.ts'",
+    update(target)
+  ]
+};
+
+rules['README.md'] = {
+  prereqs: ['lib'],
+  recipe: () => ['node lib/create-readme.js']
+};
+
+rules.lib = {
+  prereqs: ['node_modules', 'src/**/*.ts', 'tsconfig.json'],
+  recipe: target => ['yarn tsc --project .', update(target)]
+};
+
+rules.node_modules = {
+  prereqs: ['package.json', 'yarn.lock'],
+  recipe: target => ['yarn install --check-files', update(target)]
+};
+
+rules['yarn.lock'] = {
+  recipe: () => ['yarn']
+};
+
+rules.clean = {
+  phony: true,
+  recipe: () => ['rm -rf lib/']
+};
