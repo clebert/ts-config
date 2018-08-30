4dcc787d424e4445ed93521cd2f984b2cce4d929
Clemens Akens <clebert@me.com>
2018-08-31 00:17:47 +0200

    build: bump autosrc from 0.10.0 to 0.11.0

--- autosrc.js
+++ autosrc.js
@@ -5,14 +5,32 @@ const autosrc = require('autosrc');
 /** @type {autosrc.Rules} */
 const rules = (exports.rules = {});
 
-const update = target => `touch ${target}`;
+const touch = file => `touch ${file}`;
 
-rules.test = {
+const tslint = files => {
+  const tsFiles = files.filter(file => /\.tsx?$/.test(file)).join(' ');
+
+  if (tsFiles) {
+    return `yarn tslint --config tslint.json --project tsconfig.json --format verbose ${tsFiles}`;
+  }
+};
+
+rules.all = {
+  phony: true,
+  prereqs: ['format']
+};
+
+rules.clean = {
+  phony: true,
+  prereqs: ['src/**/*.{ts,tsx}'],
+  recipe: (_, newerPrereqs) => [...newerPrereqs.map(touch), 'rm -rf lib']
+};
+
+rules.format = {
   prereqs: ['README.md'],
   recipe: target => [
-    "yarn prettier --list-different '**/*.{js,json,md,ts,yml}'",
-    "yarn tslint --config tslint.json --project . --format verbose '**/*.ts'",
-    update(target)
+    "yarn prettier --list-different '**/*.{js,json,md,ts,tsx,yml}'",
+    touch(target)
   ]
 };
 
@@ -22,16 +40,20 @@ rules['README.md'] = {
 };
 
 rules.lib = {
-  prereqs: ['node_modules', 'src/**/*.ts', 'tsconfig.json'],
-  recipe: target => ['yarn tsc --project .', update(target)]
+  prereqs: [
+    'node_modules',
+    'src/**/*.{ts,tsx}',
+    'tsconfig.json',
+    'tslint.json'
+  ],
+  recipe: (target, newerPrereqs) => [
+    'yarn tsc --project tsconfig.json',
+    tslint(newerPrereqs),
+    touch(target)
+  ]
 };
 
 rules.node_modules = {
   prereqs: ['package.json', 'yarn.lock'],
-  recipe: target => ['yarn install --check-files', update(target)]
-};
-
-rules.clean = {
-  phony: true,
-  recipe: () => ['rm -rf lib']
+  recipe: target => ['yarn install --check-files', touch(target)]
 };
